8c722eecb15b405874978d0e1f0b99272638a18d
Imated <lineborder122@gmail.com>
2024-02-12 18:49:58 -0500

    Camera Stuff

--- Assets/Scripts/Camera/Camera.cs
+++ Assets/Scripts/Camera/Camera.cs
@@ -1,26 +1,43 @@
 using System;
 using Cinemachine;
+using DG.Tweening;
+using DG.Tweening.Core;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
     [SerializeField] private Player player;
+    [SerializeField] private float duration;
     private CinemachineVirtualCamera _cam;
+    private float _elapsedTime;
 
-     private void Awake()
-     {
-         _cam = GetComponent<CinemachineVirtualCamera>();
-     }
+    private void Awake()
+    {
+        _cam = GetComponent<CinemachineVirtualCamera>();
+    }
 
-     private void Update()
-     {
-         if (player.GetDir() > 0)
-             _cam.GetCinemachineComponent<CinemachineFramingTransposer>().m_TrackedObjectOffset =
-                 new Vector3(6, 3.25f, 0);
-         else if (player.GetDir() < 0)
-         {
-             _cam.GetCinemachineComponent<CinemachineFramingTransposer>().m_TrackedObjectOffset =
-                 new Vector3(-6, 3.25f, 0);
-         }
-     }
+    private void Update()
+    {
+        var framingTransposer = _cam.GetCinemachineComponent<CinemachineFramingTransposer>();
+        if (player.GetDir() > 0)
+        {
+            _elapsedTime = 0f;
+            while (_elapsedTime < duration)
+            {
+                var newAmount = Mathf.Lerp(framingTransposer.m_TrackedObjectOffset.x, 6f, _elapsedTime);
+                framingTransposer.m_TrackedObjectOffset = new Vector3(newAmount, 3.25f, 0);
+                _elapsedTime += Time.deltaTime;
+            }
+        }
+        else if (player.GetDir() < 0)
+        {
+            _elapsedTime = 0f;
+            while (_elapsedTime < duration)
+            {
+                var newAmount = Mathf.Lerp(framingTransposer.m_TrackedObjectOffset.x, -6f, _elapsedTime);
+                framingTransposer.m_TrackedObjectOffset = new Vector3(newAmount, 3.25f, 0);
+                _elapsedTime += Time.deltaTime;
+            }
+        }
+    }
 }
