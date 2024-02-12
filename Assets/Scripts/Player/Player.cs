3ff26b7d3415ea4017b41fe043a456a2e109cc88
Ηλίας Πονηρίδης <Creperi>
2024-02-13 00:03:15 +0200

    Added basic UI

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,6 +9,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float runSpeed;
     private Rigidbody2D _rb;
 
+    private List<GameObject> Items = new List<GameObject>();
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -17,5 +20,23 @@ public class Player : MonoBehaviour
         var h = Input.GetAxisRaw("Horizontal");
         var speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : movementSpeed;
         _rb.velocity = new Vector2(h * speed, _rb.velocity.y);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Door"))
+        {
+            Debug.Log("Open the door");
+        }
+        else if (other.gameObject.CompareTag("Key"))
+        {
+            Debug.Log("Get the key");
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Items.Add(other.gameObject);
+                Debug.Log("Item got");
+                Destroy(other.gameObject);
+            }
+        }
+
     }
 }
