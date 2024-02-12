e7f5298b513dfd9cdb408cf349a19cbf29778a3c
Imated <lineborder122@gmail.com>
2024-02-11 22:53:01 -0500

    Player Movement + Selecatble Mouse Over

--- Assets/Selectable.cs
+++ Assets/Selectable.cs
@@ -1,9 +1,27 @@
+using System;
 using UnityEngine;
 
 public abstract class Selectable : MonoBehaviour
 {
+    private bool _isMouseOver;
+
+    private void OnMouseEnter()
+    {
+        _isMouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        _isMouseOver = false;
+    }
+
     public bool IsPlayerClicking()
     {
         return Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
+    }
+    
+    public bool IsMouseOver()
+    {
+        return _isMouseOver;
     }
 }
