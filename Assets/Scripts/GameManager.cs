9176f9c55d1ed5b6bb2145fd2172f7a4d7ec4fe4
Imated <lineborder122@gmail.com>
2024-02-13 13:29:14 -0500

    Basic Inventory

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum GameState
@@ -12,8 +13,20 @@ public class GameManager : Singleton<GameManager>
     public GameState GameState => _gameState;
     private GameState _gameState;
 
+    private List<Selectable> _inventory = new List<Selectable>();
+
     public void SetGameState(GameState state)
     {
         _gameState = state;
+    }
+
+    public void AddToInventory(Selectable obj)
+    {
+        _inventory.Add(obj);
+    }
+
+    public bool InventoryContains(Selectable obj)
+    {
+        return  _inventory.Contains(obj);
     }
 }
