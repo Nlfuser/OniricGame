31142868f93571d53437b9c5f15c70ed1ae5ae00
Ηλίας Πονηρίδης <Creperi>
2024-02-15 00:38:35 +0200

    Added Item Pickup Script

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,7 +1,16 @@
-using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 public class Item : Selectable
 {
+    public GameObject[] IntervorySlots;
+    //public GameObject Door;
+    public void Start(){
+        IntervorySlots = GameObject.FindGameObjectsWithTag("IntervorySlot");
+        //Door = GameObject.Find("Door");
+    }
     private void Awake()
     {
         
@@ -9,10 +18,42 @@ public class Item : Selectable
 
     private void Update()
     {
-        if (IsMouseOver() && IsPlayerClicking())
+    }
+
+    private void OnMouseEnter()
+    {
+        // Collider2D DoorColider = Door.GetComponent<Collider2D>();
+        // bool isTrigger = DoorColider.isTrigger;
+        // if(isTrigger){
+
+        // }
+
+    }
+
+
+    private void OnMouseDown(){
+        Destroy(gameObject);
+    }
+
+    private void OnMouseUp(){
+        ChangeSprite();
+    }
+    void ChangeSprite()
+{
+    foreach (GameObject IntervorySlot in IntervorySlots)
+    {
+        SpriteRenderer slotSpriteRenderer = IntervorySlot.GetComponent<SpriteRenderer>();
+
+        if (slotSpriteRenderer != null)
         {
-            GameManager.instance.AddToInventory(this);
-            Destroy(gameObject);
+            IntervorySlot.GetComponent<Image>().overrideSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("SpriteRenderer component not found on IntervorySlot: " + IntervorySlot.name);
         }
     }
+}
+
+
 }
