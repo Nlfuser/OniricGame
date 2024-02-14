583e30ca5bfd3da5307f3e10d78dc6c11ddb306f
Imated <lineborder122@gmail.com>
2024-02-13 21:59:07 -0500

    Working Door Transitions + Glitch Effect

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,85 +1,12 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
 
 public class Door : Selectable
 {
-    public string sceneToLoad;
-    public float transitionDuration = 2.0f; // Set your desired transition duration here
-    public Material inkTransitionMaterial;
-    private float currentTransitionTime = 0.0f;
-
-    private bool isTransitioning = false; // Flag to check if the transition is in progress
-
-    //private void Awake()
-    //{
-    //    DontDestroyOnLoad(gameObject);
-    //}
+    [SerializeField] private string sceneToLoad;
 
     private void Update()
     {
-        if (IsMouseOver() && IsPlayerClicking() && !isTransitioning)
-        {
-            StartCoroutine(TransitionAndLoadScene());
-        }
-    }
-
-    IEnumerator TransitionAndLoadScene()
-    {
-        isTransitioning = true;
-
-        inkTransitionMaterial.SetKeyword(new LocalKeyword(inkTransitionMaterial.shader, "_LEFTSIDE"), false);
-
-        float startTime = Time.time;
-        while (currentTransitionTime < transitionDuration)
-        {
-            float t = (Time.time - startTime) / transitionDuration;
-            currentTransitionTime = Mathf.Lerp(0.0f, transitionDuration, t);
-
-            // Update the amount slider on the ink transition material
-            float amount = Mathf.Lerp(0.0f, 1.0f, currentTransitionTime / transitionDuration);
-            inkTransitionMaterial.SetFloat("_Amount", amount);
-
-            yield return null;
-        }
-
-        // Ensure the amount is set to 1 at the end of the loop
-        inkTransitionMaterial.SetFloat("_Amount", 1.0f);
-
-        StartCoroutine(TransitionOut());
-    }
-
-    IEnumerator TransitionOut()
-    {
-        float startTime = Time.time;
-        currentTransitionTime = 0.0f;
-
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
-
-        // Continue the transition while the scene is being loaded asynchronously
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
-
-        inkTransitionMaterial.SetKeyword(new LocalKeyword(inkTransitionMaterial.shader, "_LEFTSIDE"), true);
-
-        while (currentTransitionTime < transitionDuration)
-        {
-            float t = (Time.time - startTime) / transitionDuration;
-            currentTransitionTime = Mathf.Lerp(0.0f, transitionDuration, t);
-
-            // Update the amount slider on the ink transition material
-            float amount = Mathf.Lerp(1.0f, 0.0f, currentTransitionTime / transitionDuration);
-            inkTransitionMaterial.SetFloat("_Amount", amount);
-
-            yield return null;
-        }
-
-        // Ensure the amount is set to 0 at the end of the loop
-        inkTransitionMaterial.SetFloat("_Amount", 0.0f);
-
-        isTransitioning = false; // Reset the flag to allow new transitions
+        if (IsMouseOver() && IsPlayerClicking() && !SceneManager.instance.IsTransitioning())
+            SceneManager.instance.StartTransition(sceneToLoad);
     }
 }
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using static UnityEngine.SceneManagement.SceneManager;
+
+public class SceneManager : Singleton<SceneManager>
+{
+    [SerializeField] private float transitionDuration = 2.0f; // Set your desired transition duration here
+    [SerializeField] private Material inkTransitionMaterial;
+    private bool _isTransitioning = false;
+    private static readonly int Amount = Shader.PropertyToID("_Amount");
+    private static LocalKeyword _leftSide;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+        _leftSide = new LocalKeyword(inkTransitionMaterial.shader, "_LEFTSIDE");
+        sceneLoaded += TransitionOut;
+    }
+
+    public void StartTransition(string sceneName)
+    {
+        if(_isTransitioning)
+            return;
+        _isTransitioning = true;
+        inkTransitionMaterial.SetKeyword(_leftSide, false);
+        inkTransitionMaterial.SetFloat(Amount, 0);
+        
+        StartCoroutine(Transition(0f, 1f, () => {
+            LoadScene(sceneName);
+        }));
+    }
+    
+    private void TransitionOut(Scene scene, LoadSceneMode mode)
+    {
+        if(!_isTransitioning)
+            return;
+        inkTransitionMaterial.SetKeyword(_leftSide, true);
+        inkTransitionMaterial.SetFloat(Amount, 1);
+
+        StartCoroutine(Transition(1f, 0f, () => _isTransitioning = false));
+    }
+
+    private IEnumerator Transition(float from, float to, Action onComplete = null)
+    {
+        var startTime = Time.time;
+        while (Time.time - startTime < transitionDuration)
+        {
+            var t = (Time.time - startTime) / transitionDuration;
+            var amount = Mathf.Lerp(from, to, t);
+            inkTransitionMaterial.SetFloat(Amount, amount);
+            yield return null;
+        }
+
+        inkTransitionMaterial.SetFloat(Amount, to);
+        
+        onComplete?.Invoke();
+    }
+
+
+    public bool IsTransitioning()
+    {
+        return _isTransitioning;
+    }
+}
