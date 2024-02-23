9487ca0c5cad4391fd1f0ad07c250a79d67ade2f
Imated <lineborder122@gmail.com>
2024-02-22 21:40:40 -0500

    Walk In Doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,12 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Door : Selectable
 {
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private bool walkThrough;
 
     private void Update()
     {
-        if (IsMouseOver() && IsPlayerClicking() && !SceneManager.instance.IsTransitioning())
+        if (!walkThrough && IsMouseOver() && IsPlayerClicking() && !SceneManager.instance.IsTransitioning())
+            SceneManager.instance.StartTransition(sceneToLoad);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(walkThrough && col.CompareTag("Player") && !SceneManager.instance.IsTransitioning())
             SceneManager.instance.StartTransition(sceneToLoad);
     }
 }
